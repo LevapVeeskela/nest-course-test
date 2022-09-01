ccc90198c174310763fcbcea77bd8f0eda55ebdd
Veeskela <pavel.alekseev@ecomcharge.com>
2022-09-01 07:03:35 +0300

    +*[NCT] - added constants, create-review.dto for review entity and fill logic review service by GRUD

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,6 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { ModelType, DocumentType } from '@typegoose/typegoose/lib/types';
+import { DeleteResult } from 'mongodb';
+import { Types } from 'mongoose';
+import { InjectModel } from 'nestjs-typegoose';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { ReviewModel } from './review.model';
 
 @Injectable()
 export class ReviewService {
-    
+    public get reviewModel(): ModelType<ReviewModel> {
+        return this._reviewModel;
+    }
+    constructor(
+        @InjectModel(ReviewModel)
+        private readonly _reviewModel: ModelType<ReviewModel>,
+    ) { }
+
+    async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
+        return this.reviewModel.create(dto);
+    }
+
+    async delete(id: string): Promise<DocumentType<ReviewModel> | null> {
+        return this.reviewModel.findByIdAndDelete(id).exec();
+    }
+
+    async findByProductId(
+        productId: string,
+    ): Promise<DocumentType<ReviewModel>[]> {
+        return this.reviewModel
+            .find({ productId: new Types.ObjectId(productId) })
+            .exec();
+    }
+
+    async deleteByProductId(productId: string): Promise<DeleteResult> {
+        return this.reviewModel
+            .deleteMany({ productId: new Types.ObjectId(productId) })
+            .exec();
+    }
 }
